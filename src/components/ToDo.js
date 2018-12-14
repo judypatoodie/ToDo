34477167b99fe9c585166b40a88039e63107d87f
Judy Huynh <jhuynh@mossbeachhomes.com>
2018-12-14 13:01:06 -0800

    added delete button to app

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,9 +4,10 @@ class ToDo extends Component {
   render() {
     return (
       <li>
-      <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-         <span>{ this.props.description }</span>
-       </li>
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{ this.props.description }</span>
+        <input type="button" value="delete" style={{marginLeft: '20px'}} onClick={ this.props.handleDelete } />
+      </li>
     );
   }
 }
